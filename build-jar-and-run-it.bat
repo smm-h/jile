2531fcc4600086629535ece173bfa9c9c95e6135
SMMH <smmh72@gmail.com>
2021-08-21 12:11:21 +0430

    final touches before departure to gradle-built nlp

--- build-jar-and-run-it.bat
+++ build-jar-and-run-it.bat
@@ -4,6 +4,12 @@ cd jarbuilder
 set package=jile.lingu
 set mainclass=Codestack
 
+@REM set package=jile.vis
+@REM set mainclass=ClassTree
+
+@REM set package=jile.common
+@REM set mainclass=PercentDecoder
+
 python package-to-jar.py %package% %mainclass%
 python jar-runner.py ../jar/%package%.jar
 
